417269d92c88e2b2aebcdecfbc3225e8f62b9c5b
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-18 11:59:00 -0300

    Alterando texto do tvNome

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_18_05_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_18_05_2021/MainActivity.kt
@@ -2,10 +2,15 @@ package com.oceanbrasil.ocean_android_introducao_18_05_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvNome = findViewById<TextView>(R.id.tvNome)
+
+        tvNome.text = "Novo texto"
     }
 }
