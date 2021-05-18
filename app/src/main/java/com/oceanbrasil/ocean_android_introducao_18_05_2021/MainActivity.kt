a9ec397eba26b4fc76a1a5700a7161a43a272650
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-18 12:18:51 -0300

    Resolução do exercício

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_18_05_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_18_05_2021/MainActivity.kt
@@ -2,6 +2,8 @@ package com.oceanbrasil.ocean_android_introducao_18_05_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val tvNome = findViewById<TextView>(R.id.tvNome)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.etNome)
 
-        tvNome.setOnClickListener {
-            tvNome.text = "Novo texto"
+        btEnviar.setOnClickListener {
+            tvNome.text = etNome.text
         }
 
         /*
